bdfafc78009da15ad2c6fa76329992f418b5c733
liuxin85 <liuxin85@gmail.com>
2026-02-10 16:24:08 +0800

    feat: subscribe the realtime data

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,24 +1,103 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase-client";
+import { Chart } from "react-charts";
 
 function Dashboard() {
-  const fetchMetrics = async () => {
-    const response = await supabase.from("sales_deals").select(
-      `
-    name,
-    value.sum()
-    `,
-    );
-    console.log(response);
-  };
+  const [metrics, setMetrics] = useState([]);
 
   useEffect(() => {
     fetchMetrics();
+
+    const channel = supabase
+      .channel("deal-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "sales_deals",
+        },
+        (payload) => {
+          console.log(payload.new);
+          fetchMetrics();
+        },
+      )
+      .subscribe();
+
+    // Clean up subscription
+    return () => {
+      supabase.removeChannel(channel);
+    };
   }, []);
+
+  const fetchMetrics = async () => {
+    try {
+      const { data, error } = await supabase.from("sales_deals").select(
+        `
+    name,
+    value.sum()
+    `,
+      );
+
+      if (error) {
+        throw error;
+      }
+      console.log(data);
+      setMetrics(data);
+    } catch (error) {
+      console.error("Error fetching metrics:", error);
+    }
+  };
+  const chartData = [
+    {
+      data: metrics.map((m) => ({ primary: m.name, secondary: m.sum })),
+    },
+  ];
+  const primaryAxis = {
+    getValue: (d) => d.primary,
+    scaleType: "band",
+    padding: 0.2,
+    position: "bottom",
+  };
+  const secondaryAxes = [
+    {
+      getValue: (d) => d.secondary,
+      scaleType: "linear",
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ];
+  function y_max() {
+    if (metrics?.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum));
+      return maxSum + 2000;
+    }
+
+    return 5000;
+  }
+
   return (
     <div className="dashbaord-wrapper">
       <div className="chart-containter">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1, height: "400px", width: "500px" }}>
+          <Chart
+            options={{
+              data: chartData,
+              primaryAxis,
+              secondaryAxes,
+              type: "bar",
+              defaultColors: ["#58d675"],
+              tooltip: {
+                show: false,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
